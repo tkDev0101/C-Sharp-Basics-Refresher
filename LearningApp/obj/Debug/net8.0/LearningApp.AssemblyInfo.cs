3d2243f46816c0f9b7ba52ad0f5656baaefdf492
tkDev <tkdev0101@gmail.com>
2025-06-26 20:34:44 +0200

    code Update

--- LearningApp/obj/Debug/net8.0/LearningApp.AssemblyInfo.cs
+++ LearningApp/obj/Debug/net8.0/LearningApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bb29bc0274c555db25003db433ece67fe5f52f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e5e0837ab645242f979ceff057c06dddc57083")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
